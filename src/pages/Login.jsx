153f2526b2fe78ed61bef3abc615249a9a54f805
Nico :D <nicopereyra855@gmail.com>
2025-03-11 09:04:06 -0300

    Arreglo de tama;o en login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,21 @@
 import LoginForm from "../components/LoginForm";
 import back from "../assets/flecha-atras.png";
 import { Link } from "react-router-dom";
-import imgForm from "../assets/imgForm.jpg"
+import imgForm from "../assets/imgForm.jpg";
 
 export default function Login() {
   return (
     <main data-aos="fade-in">
       <a href="/">
         <img
-          className="w-[40px] w-[40px] absolute top-10 left-10"
+          className="w-[40px] h-[40px] absolute top-10 left-10"
           src={back}
           alt="back icon"
         />
       </a>
 
       <div className="w-full min-h-screen bg-[var(--wine-color)] flex justify-center items-center">
-        <div className="flex flex-col w-[85%] max-w-[1000px] md:flex-row lg:w-[75%] rounded-lg flex justify-center">
+        <div className="flex flex-col w-[85%] h-[70vh] md:min-h-[550px] md:flex-row lg:w-[75%] rounded-lg flex justify-center">
           <div className="w-[40%]">
             <img
               className="h-full w-full object-cover overflow-hidden rounded-l-lg hidden md:block"
